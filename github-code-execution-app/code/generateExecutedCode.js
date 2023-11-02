092694243b8069e94741a7b0d9c2eb8e80df8ad6
ritesh22201 <riteshgoswami22201@gmail.com>
2023-11-02 08:33:36 +0530

    code added

--- github-code-execution-app/code/generateExecutedCode.js
+++ github-code-execution-app/code/generateExecutedCode.js
@@ -2,8 +2,6 @@ const { DiscussServiceClient } = require("@google-ai/generativelanguage");
 const { GoogleAuth } = require("google-auth-library");
 require('dotenv').config();
 
-
-const MODEL_NAME = "models/chat-bison-001";
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 const client = new DiscussServiceClient({
@@ -12,7 +10,7 @@ const client = new DiscussServiceClient({
 
 const generateExecutedCode = async (code) => {
     const result = await client.generateMessage({
-        model : MODEL_NAME,
+        model : "models/chat-bison-001",
         prompt : {
             messages : [{content : `Execute this code ${code} and generate the probable output. Please generate only the code output after executing, do not generate the explanation.`}]
         }
